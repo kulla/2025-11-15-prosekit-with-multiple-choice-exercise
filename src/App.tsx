0b314fd7dfa04247b4706284d25c7c6dbd7539ac
Stephan Kulla <git.mail@kulla.me>
2025-11-14 18:34:39 +0100

    feat: Add editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import '@picocss/pico/css/pico.min.css'
+import 'prosekit/basic/style.css'
+import 'prosekit/basic/typography.css'
 import './App.css'
 
-export default function App() {
+import { defineBasicExtension } from 'prosekit/basic'
+import { createEditor } from 'prosekit/core'
+import { ProseKit } from 'prosekit/react'
+import { useMemo } from 'react'
+
+export default function Editor() {
+  const editor = useMemo(() => {
+    const extension = defineBasicExtension()
+    return createEditor({ extension })
+  }, [])
+
   return (
-    <main className="p-10">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+    <main>
+      <h1>Editor</h1>
+      <ProseKit editor={editor}>
+        <div ref={editor.mount} className="outline-solid p-4"></div>
+      </ProseKit>
     </main>
   )
 }

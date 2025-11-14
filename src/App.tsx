067cfc28013302b1bfd76294e0c930b7f0dc4a30
Stephan Kulla <git.mail@kulla.me>
2025-11-14 18:55:31 +0100

    feat: Add basic MultipleChoiceExercise node

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,29 @@ import 'prosekit/basic/typography.css'
 import './App.css'
 
 import { defineBasicExtension } from 'prosekit/basic'
-import { createEditor } from 'prosekit/core'
+import { createEditor, defineNodeSpec, insertNode, union } from 'prosekit/core'
 import { ProseKit } from 'prosekit/react'
-import { useMemo } from 'react'
+import { useCallback, useMemo } from 'react'
 
 export default function Editor() {
   const editor = useMemo(() => {
-    const extension = defineBasicExtension()
+    const extension = union(
+      defineBasicExtension(),
+      defineMultipleChoiceExerciseSpec(),
+    )
     return createEditor({ extension })
   }, [])
+
+  const handleAddMultipleChoiceExercise = useCallback(() => {
+    editor.exec(insertNode({ type: 'multipleChoiceExercise' }))
+  }, [editor])
 
   return (
     <main>
       <h1>Editor</h1>
+      <button type="button" onClick={handleAddMultipleChoiceExercise}>
+        Add MultipleChoiceExercise
+      </button>
       <ProseKit editor={editor}>
         <div
           ref={editor.mount}
@@ -26,4 +36,16 @@ export default function Editor() {
       </ProseKit>
     </main>
   )
+}
+
+function defineMultipleChoiceExerciseSpec() {
+  return defineNodeSpec({
+    name: 'multipleChoiceExercise',
+    content: 'paragraph+',
+    group: 'block',
+    parseDOM: [{ tag: 'div.multiple-choice-exercise' }],
+    toDOM() {
+      return ['div', { class: 'multiple-choice-exercise' }, 0]
+    },
+  })
 }
